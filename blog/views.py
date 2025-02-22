e261388cb3d1e883585c68f305a0a601d2100d62
kay <sergkadet@yandex.ru>
2025-02-22 16:46:02 +0300

    development of views, url templates and html templates for the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(requset, id):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("Постов нету")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(requset, 'blog/post/detail.html', {'post': post})
+
