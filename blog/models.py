d5583968d9d3cb34dccad4e0061f31e89c1e2093
kay <sergkadet@yandex.ru>
2025-02-26 16:45:17 +0300

    creating canonical urls and pagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -11,7 +12,9 @@ class Post(models.Model):
         DRAFT = 'DT', 'Черновик'
         PUBLISHED = 'PB', 'Опубликован'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -36,3 +39,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail", 
+            args=[self.publish.year,
+                  self.publish.month,
+                  self.publish.day,
+                  self.slug
+            ]
+    )    
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 # from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(requset, id):
+def post_detail(requset, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("Постов нету")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post, 
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(requset, 'blog/post/detail.html', {'post': post})
 
